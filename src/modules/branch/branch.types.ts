47f14b1127e8671188cb676208f913c3d573cc38
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-03-06 16:31:00 +0300

    Добавил отзывы , квиз , доработал карточки

---
+++ src/modules/branch/branch.types.ts
@@ -0,0 +1,3 @@
+export interface BranchProps {
+  className?: string
+}
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -4,11 +4,8 @@ import classNames from 'classnames'
 
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
-import { FormFirst } from '@/modules/formFirst'
 import { Favour, Introduce, Treaty } from '@/components'
 import { FormSecond } from '@/modules/formSecond'
-import { Tenet } from '@/modules/tenet'
-import { Conversion } from '@/modules/conversion'
 import { Case } from '@/modules/case'
 // import { DrawerMenu } from '@/modules/drawerMenu'
 import NewModalContainer from '../../components/newModalContainer/newModalContainer'
@@ -17,6 +14,9 @@ import Faq from '@/modules/faq/faq'
 import Clients from '@/modules/clients/clients'
 import Why from '@/modules/why/why'
 import HowWeWork from '@/modules/howwework/howwework'
+import Quiz from '@/modules/quiz/quiz'
+import Branch from '@/modules/branch/branch'
+import Review from '@/modules/review/review'
 
 const Home: FC<HomeProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
@@ -28,15 +28,14 @@ const Home: FC<HomeProps> = ({ className }) => {
         <Introduce />
         <Favour />
         <HowWeWork />
+        <Branch />
         <RedBoxWork />
         <Case />
-        <FormFirst />
-        <Conversion />
-        <Treaty />
-        <Tenet />
         <Why />
         <Clients />
+        <Quiz />
         <Faq />
+        <Review />
         <FormSecond />
       </Wrapper>
       {/* <DrawerMenu /> */}
